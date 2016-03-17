48724aec0210bb8c2a7648d5b3ff3e27f59d5eed
Pierre Tondereau <ptondereau@doers.fr>
2016-03-17 23:44:29 +0100

    Initail commit
    Initial commit
    
    Initial Commit

---
+++ src/UpsApiServiceProvider.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Rooferz\LaravelUpsApi;
+
+/**
+ * This is the Ups Api service provider class.
+ *
+ * @author Pierre Tondereau <pierre@doers.fr>
+ */
+class UpsApiServiceProvider
+{
+
+}
